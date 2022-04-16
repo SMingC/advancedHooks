dff20955ee4455b3c4e7499327d336a0a7518327
Smc <347552878@qq.com>
2022-04-16 13:56:32 +0800

    section3--15%

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,50 @@
 import React from "react"
 import styled from "styled-components"
+import FlutterBuild from "../components/builds/FlutterBuild"
+
 import PurchaseButton from "../components/buttons/PurchaseButton"
+import CourseCard from "../components/cards/CourseCard"
 const IndexPage = () => (
   <Wrapper>
-    <TextWrapper>
-      <Logo src="/images/logos/react-logo.svg" alt="logo" />
-      <Title>Build a web app with React Hooks</Title>
-      <Caption>20 sections - 3 hours of videos</Caption>
-      <Description>
-        Learn how we built the new Design+Code site with React Hooks using
-        Gatsby, Netlify, and advanced CSS techniques with Styled Components.
-      </Description>
-      <AuthorWrapper>
-        <AvatarImage src="/images/avatars/Meng.png" alt="avatar" />
-        <Caption>Taught by Meng To</Caption>
-      </AuthorWrapper>
-      <PurchaseButton />
-      <SmallText>
-        Purchase includes access to 30 courses. Over 80 hours of content,
-        including 12 hours for SwiftUI, for iOS 13 and iOS 14.
-      </SmallText>
-    </TextWrapper>
+    <HeroWrapper>
+      <CourseCard />
+      <TextWrapper>
+        <Logo src="/images/logos/react-logo.svg" alt="logo" />
+        <Title>Build a web app with React Hooks</Title>
+        <Caption>20 sections - 3 hours of videos</Caption>
+        <Description>
+          Learn how we built the new Design+Code site with React Hooks using
+          Gatsby, Netlify, and advanced CSS techniques with Styled Components.
+        </Description>
+        <AuthorWrapper>
+          <AvatarImage src="/images/avatars/Meng.png" alt="avatar" />
+          <Caption>Taught by Meng To</Caption>
+        </AuthorWrapper>
+        <PurchaseButton />
+        <SmallText>
+          Purchase includes access to 30 courses. Over 80 hours of content,
+          including 12 hours for SwiftUI, for iOS 13 and iOS 14.
+        </SmallText>
+      </TextWrapper>
+    </HeroWrapper>
+    <FlutterBuild />
   </Wrapper>
 )
 
 export default IndexPage
 
 const Wrapper = styled.div`
-  height: 1247px;
   background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
 `
 const TextWrapper = styled.div`
   position: relative;
   display: grid;
   gap: 20px;
-  padding-top: 220px;
+
+  @media (max-width: 780px) {
+    justify-items: center;
+    text-align: center;
+  }
 `
 const AuthorWrapper = styled.div`
   display: flex;
@@ -42,15 +52,30 @@ const AuthorWrapper = styled.div`
   gap: 16px;
 `
 
+const HeroWrapper = styled.div`
+  display: grid;
+  max-width: 1234px;
+  grid-template-columns: 360px auto;
+  gap: 60px;
+  padding: 220px 20px 0px;
+  justify-content: center;
+  margin: 0 auto;
+
+  @media (max-width: 780px) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
+`
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`
+
 const AvatarImage = styled.img`
   width: 32px;
   height: 32px;
 `
 
-const Logo = styled.img`
-  width: 60px;
-  height: 60px;
-`
 const Title = styled.h1`
   max-width: 500px;
   font-style: normal;
